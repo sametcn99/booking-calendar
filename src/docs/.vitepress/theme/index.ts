ad761e016831d893f0b55ec2b1b14be43a023c9d
samet <sametcn99@gmail.com>
2026-02-22 01:12:53 +0300

    refactor: remove unused image viewer import and enhanceApp method

--- src/docs/.vitepress/theme/index.ts
+++ src/docs/.vitepress/theme/index.ts
@@ -4,8 +4,6 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { h } from "vue";
 import "./style.css";
-import ImageViewerP from "@miletorix/vitepress-image-viewer";
-import "@miletorix/vitepress-image-viewer/style.css";
 
 export default {
 	extends: DefaultTheme,
@@ -13,8 +11,5 @@ export default {
 		return h(DefaultTheme.Layout, null, {
 			// https://vitepress.dev/guide/extending-default-theme#layout-slots
 		});
-	},
-	enhanceApp(ctx) {
-		ImageViewerP(ctx.app);
 	},
 } satisfies Theme;
